20a9d9f43956d102cb62ee0e7eceae9ac9de86d9
Jorge <jlarroyop@gmail.com>
2026-09-02 08:10:59 -0600

    Implemente code rabbit suggested fixes

--- components/UploadForm.tsx
+++ components/UploadForm.tsx
@@ -118,7 +118,7 @@ const UploadForm = () => {
             if (book.alreadyExists) {
                 toast.info("Book with same title already exists.");
                 form.reset();
-                router.push(`/books/${existsCheck.book.slug}`);
+                router.push(`/books/${book.data.slug}`);
                 return;
             }
 
